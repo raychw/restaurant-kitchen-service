119d5d25100bb45cc243473ba8006b839d3d8906
reichikku <bladeryz@gmail.com>
2024-08-31 16:46:53 +0300

    implemented sessions and a page visits counter

--- catalog/views.py
+++ catalog/views.py
@@ -11,10 +11,15 @@ def index(request):
     num_dishes = Dish.objects.all().count()
     num_cooks = Cook.objects.all().count()
 
+    num_visits = request.session.get("num_visits", 0)
+    num_visits += 1
+    request.session["num_visits"] = num_visits
+
     context = {
         "num_dish_types": num_dish_types,
         "num_dishes": num_dishes,
         "num_cooks": num_cooks,
+        "num_visits": num_visits,
     }
 
     return render(request, "catalog/index.html", context=context)
