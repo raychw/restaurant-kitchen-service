1c009f0905f9cb88ffd5d848fb7690b89badf49d
reichikku <bladeryz@gmail.com>
2024-09-02 12:03:32 +0300

    fixed up IngredientListView and added html templates

--- catalog/views.py
+++ catalog/views.py
@@ -185,7 +185,7 @@ class IngredientListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(Ingredient, self).get_context_data(**kwargs)
+        context = super(IngredientListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
 
         context["search_form"] = IngredientNameSearchForm(
