54b3393b80b29cb15ef5a3e5834915548894c59f
reichikku <bladeryz@gmail.com>
2024-09-02 11:53:23 +0300

    added new model ingredient with many-to-many connection to dish

--- catalog/models.py
+++ catalog/models.py
@@ -38,12 +38,20 @@ class Cook(AbstractUser):
         return reverse("catalog:cook_detail", kwargs={"pk": self.pk})
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
 
     def __str__(self):
         return self.name
